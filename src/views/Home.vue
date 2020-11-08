3f08ee0ea64e1568a91dd2c36777ee9a3726f28b
yu_horse@icloud.com <a18171y@reitaku.jp>
2020-11-09 00:05:28 +0900

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <h1>郵便番号から該当地域を検索する</h1>
-    <input v-model="postcode" type="text" maxlength="7" placeholder="郵便番号を入力" />
+    <input
+      v-model="postcode"
+      type="text"
+      maxlength="7"
+      placeholder="郵便番号を入力"
+    />
     <button @click="searchAdressInfo" type="submit">検索</button>
     <p>※ 3桁以上の数値を入力してください（例：1500001）</p>
     <h2>検索結果：{{ allAdress }}</h2>
@@ -14,18 +19,17 @@ export default {
   data() {
     return {
       postcode: "",
-      allAdress: ""
+      allAdress: "",
     };
   },
-  async created() {
-    let item = await axios.get(
-      `https://apis.postcode-jp.com/api/v4/postcodes/${this.postcode} \
-      -G -v \
-      -d "fields=allAddress"`
-    );
-    this.data = item.data;
-    this.postcode = this.data.allAdress;
-  }
+  methods: {
+    async searchAdressInfo() {
+      let item = await axios.get(
+        `https://apis.postcode-jp.com/api/v4/postcodes/${this.postcode}?apiKey=QEX6w8u2EMpeZz2c49aFiwhJZOC0ftiH2ILV0Yr`
+      );
+      this.allAdress = item.data[0].allAddress;
+    },
+  },
 };
 </script>
 
